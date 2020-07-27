668dca8046023352dcccd93ac82f771fe05b71a4
Joan Jara Bosch <jaraboschj@gmail.com>
2020-07-27 19:28:02 +0000

    Arreglat script i automatització dels servidors i la pagina en local

--- autoMonitoritzacio.sh
+++ autoMonitoritzacio.sh
@@ -8,33 +8,14 @@
 #Instal·lació dels paquets necessaris
 sudo apt update -y
 #sudo apt upgrade -y
-sudo apt install python3-pip python3.8 gunicorn nginx python3-django gettext -y
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev python3-dev -y
-#Instalarem el virtualenv per a poguer tindre un entorn virtual 
-#i no molestar els altres projectes que puguem tindre en un futurgettext 
+sudo apt install python3-pip python3 nginx python3-django gettext -y
+#Instalarem el virtualenv per a poguer tindre un entorn virtual
+#i no molestar els altres projectes que puguem tindre en un futurgettext
 sudo pip3 install --upgrade setuptools
 sudo pip3 install virtualenv wheel
-sudo apt purge python2.7 -y
+sudo pip3 install --upgrade django
+sudo pip3 install gunicorn
 descarregues=$(pwd)
-#crearem la carpeta al directori home del usuari odroid
-mkdir ~/aux
-cd ~/aux
-#creem el nosytre entorn virtual
-virtualenv odroidenv
-#I l'activem
-source odroidenv/bin/activate
-
-#Al arxiu RAR, tenim un fitxer anomenat requirements, on tenim tots els 
-#requeriments per al nostre porejecte i que amb la següent comanda, 
-#s'insta·laran
-
-pip3 install -r $descarregues/requirements.txt --timeout 60 --no-deps 
-
-# Ara el q farem sera copiar el nostre entorn virtual al nostre projecte 
-#descomprimi
-sudo cp -r ~/aux/odroidenv $descarregues/odroid/
-sudo rm -r ~/aux
-
 #Fem les migracions pertinents
 cd $descarregues/odroid
 python3 manage.py makemigrations
@@ -55,6 +36,5 @@ sudo rm -r /etc/nginx/sites-enabled/default
 sudo cp -p $descarregues/odroid_site /etc/nginx/sites-available/odroid_site
 sudo ln -s /etc/nginx/sites-available/odroid_site /etc/nginx/sites-enabled
 sudo systemctl restart nginx
-
 
 echo "Instal·lació completada, si obre un navegador, hauria de poguer veure anant a 0.0.0.0:8000 la nostra pagina web en django per monitoritzar les plaques odroid"
