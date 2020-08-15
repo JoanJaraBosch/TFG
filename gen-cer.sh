25f5f4e5e131ac5f1278ef6555bfa206e28331c8
Joan Jara Bosch <jaraboschj@gmail.com>
2020-08-15 22:42:55 +0200

    Creats els scripts per la seguretat, falta manual i logrotate per guardar els logs de manera eficient i ocupar menys espai

--- gen-cer.sh
+++ gen-cer.sh
@@ -26,30 +26,20 @@ then
 fi
 
 echo "Generating key request for $domain"
-
-openssl genrsa -des3 -passout pass:$password -out /etc/nginx/ssl/nginx..key 2048 -noout
-
-#Remove passphrase from the key. Comment the line out to keep the passphrase
-echo "Removing passphrase from key"
-openssl rsa -in /etc/nginx/ssl/nginx..key -passin pass:$password -out /etc/nginx/ssl/nginx..key
-
-#Create the request
-echo "Creating CSR"
-openssl req -new -key /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -passin pass:$password \
-    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email" -out /etc/nginx/ssl/nginx.crt 
 
 echo "---------------------------"
-echo "-----Below is your CSR-----"
+echo "-----Below is your CRT-----"
 echo "---------------------------"
 echo
-cat /etc/nginx/ssl/nginx.csr
+cat /etc/nginx/ssl/nginx.crt
 
 echo
 echo "---------------------------"
 echo "-----Below is your Key-----"
 echo "---------------------------"
 echo
-cat /etc/nginx/ssl/nginx.crt
+cat /etc/nginx/ssl/nginx.key
 
-openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096&
+openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
 
