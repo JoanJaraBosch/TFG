98ee787243c8f565bfcc799082dca2d3ff5a6873
Joan Jara Bosch <jaraboschj@gmail.com>
2020-09-22 18:39:56 +0200

    He afegit una comanda al fitxer de monitoring per a fer un grafic de temperatura

--- monitoring.sh
+++ monitoring.sh
@@ -4,10 +4,14 @@ if [ "$1" == "master" ]; then
 	cpu=$(ps -A -o pcpu | tail -n+2 | paste -sd+ | bc)
 	mem=$(free -m | awk '{print $7}' | head -2 | tail -1)
 	mem_avail=$(df -t ext4 --output=used,avail | head -2 | tail -1)
+	temp=$(sensors | tail -2 | head -1 | cut -d " " -f 9,13)        
 else
 	cpu=$(ssh -o StrictHostKeyChecking=no $1 "ps -A -o pcpu | tail -n+2 | paste -sd+ | bc")
         mem=$(ssh -o StrictHostKeyChecking=no $1 "free -m | awk '{print \$7}' | head -2 | tail -1")
 	mem_avail=$(ssh -o StrictHostKeyChecking=no $1 "df -t ext4 --output=used,avail | head -2 | tail -1")
+	temp=$(ssh -o StrictHostKeyChecking=no $1 "sensors | tail -2 | head -1 | cut -d \" \" -f 9,13")
 fi
 
-echo '{"cpu": "'"$cpu"'", "mem":"'"$mem"'", "mem_avail":"'"$mem_avail"'"}'
+temp=$(echo "${temp//[+,),C,°]/}")
+
+echo '{"cpu": "'"$cpu"'", "mem":"'"$mem"'", "mem_avail":"'"$mem_avail"'", "temp": "'"$temp"'"}'
