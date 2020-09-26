e8b6a5b0f4b687a99fc1db433cf96d81f15e2560
Joan Jara Bosch <jaraboschj@gmail.com>
2020-09-26 18:59:49 +0200

    He afegit un grafic de uptime

--- monitoring.sh
+++ monitoring.sh
@@ -8,7 +8,7 @@ if [ "$1" == "master" ]; then
         load=$(uptime | cut -d " " -f 11-)
 	cpunum=$(lscpu | head -3 | tail -1 | cut -d " " -f 15-)
 	entropy=$(cat /proc/sys/kernel/random/entropy_avail)
-	up=$(uptime -p)
+	up=$(uptime -p | tr -s ' ')
 else
 	cpu=$(ssh -o StrictHostKeyChecking=no $1 "ps -A -o pcpu | tail -n+2 | paste -sd+ | bc")
         mem=$(ssh -o StrictHostKeyChecking=no $1 "free -m | awk '{print \$7}' | head -2 | tail -1")
@@ -17,11 +17,13 @@ else
 	load=$(ssh -o StrictHostKeyChecking=no $1 "uptime | cut -d \" \" -f 12-")
 	cpunum=$(ssh -o StrictHostKeyChecking=no $1 "lscpu | head -3 | tail -1 | cut -d \" \" -f 15-")
 	entropy=$(ssh -o StrictHostKeyChecking=no $1 "cat /proc/sys/kernel/random/entropy_avail")
-	up=$(ssh -o StrictHostKeyChecking=no $1 "uptime -p")
+	up=$(ssh -o StrictHostKeyChecking=no $1 "uptime -p | tr -s \' \'")
 fi
 
 temp=$(echo "${temp//[+,),C,°]/}")
 load=$(echo "${load//\,/} $cpunum")
 up=$(echo "${up//[a-z,\,]/}")
+up=$(echo $up | tr -s " ")
+up=$(echo "${up//\ /-}")
 
 echo '{"cpu": "'"$cpu"'", "mem":"'"$mem"'", "mem_avail":"'"$mem_avail"'", "temp": "'"$temp"'", "load": "'"$load"'", "entropy": "'"$entropy"'", "up": "'"$up"'"}'
