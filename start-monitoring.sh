0d2f4aafbb7f898b792794638840203283f8b0c5
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-10-16 15:45:31 +0200

    Arreglat error del npm (crec)

--- start-monitoring.sh
+++ start-monitoring.sh
@@ -27,6 +27,7 @@ mkdir -p /home/odroid/.npm-global/
 mkdir -p /home/odroid/.django-monitor/
 mkdir -p /home/odroid/.pm2/
 npm config set prefix '/home/odroid/.npm-global'
+echo "export NODE_PATH=/home/odroid/.npm-global/lib/node_modules" >> /home/odroid/.profile
 echo "export PATH=/home/odroid/.npm-global/bin:$PATH" >> /home/odroid/.profile
 source /home/odroid/.profile
 chown -R odroid:odroid /home/odroid/.pm2/
@@ -83,18 +84,18 @@ echo "-------------------EXECUTE AS ODROID USER THE FILE PM2.SH-----------------
 cd /home/odroid/.django-monitor
 
 runuser -l odroid -c 'npm install os -g' 
+runuser -l odroid -c 'npm install utf-8-validate -g'
+runuser .l odroid -c 'npm install bufferutil -g'
 runuser -l odroid -c 'npm install websocket -g'
 runuser -l odroid -c 'npm install child_process -g' 
 runuser -l odroid -c 'npm install http -g' 
 runuser -l odroid -c 'npm install pm2 -g'
 
 #We make the node server up like a deamon.
-hown -R odroid:odroid /home/odroid/.django-monitor/*
-chown odroid:odroid /home/odroid/.pm2/rpc.sock /home/odroid/.pm2/pub.sock
+chown -R odroid:odroid /home/odroid/.django-monitor/*
 chown -R odroid:odroid /home/odroid/.pm2/*
-chown odroid:odroid /home/odroid/.pm2/rpc.sock /home/odroid/.pm2/pub.sock
+runuser -l odroid -c 'rm -rf ~/.pm2'
 env PATH=$PATH:/usr/bin /home/odroid/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u odroid --hp /home/odroid
-runuser -l odroid -c 'export NODE_PATH=/home/odroid/.npm-global/lib/node_modules'
 runuser -l odroid -c 'pm2 start /home/odroid/.django-monitor/servidor.js'
 runuser -l odroid -c 'pm2 startup'
 runuser -l odroid -c 'pm2 save'
