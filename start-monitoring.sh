b4079dc46282fff0afbf768aad0331465c47cd3e
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-10-16 15:12:18 +0200

    Afegit link del david a descarregues

--- start-monitoring.sh
+++ start-monitoring.sh
@@ -89,17 +89,19 @@ runuser -l odroid -c 'npm install http -g'
 runuser -l odroid -c 'npm install pm2 -g'
 
 #We make the node server up like a deamon.
+hown -R odroid:odroid /home/odroid/.django-monitor/*
+chown odroid:odroid /home/odroid/.pm2/rpc.sock /home/odroid/.pm2/pub.sock
+chown -R odroid:odroid /home/odroid/.pm2/*
 chown odroid:odroid /home/odroid/.pm2/rpc.sock /home/odroid/.pm2/pub.sock
 env PATH=$PATH:/usr/bin /home/odroid/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u odroid --hp /home/odroid
 runuser -l odroid -c 'export NODE_PATH=/home/odroid/.npm-global/lib/node_modules'
-
 runuser -l odroid -c 'pm2 start /home/odroid/.django-monitor/servidor.js'
 runuser -l odroid -c 'pm2 startup'
 runuser -l odroid -c 'pm2 save'
 
 
 echo "-------------------INSTALATION FINISHED ENJOY-------------------"
-chown -R odroid:odroid /home/odroid/.django-monitor/*
-chown odroid:odroid /home/odroid/.pm2/rpc.sock /home/odroid/.pm2/pub.sock
+
+#We restart the deamons because there are cases were the instalation needs a second restart
 systemctl restart nginx
 systemctl restart gunicorn
