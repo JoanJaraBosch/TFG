6f21642fc74e6b5cd7114160d1276f5a21024dff
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-10-18 19:24:36 +0200

    Update start-monitoring.sh

--- start-monitoring.sh
+++ start-monitoring.sh
@@ -26,9 +26,9 @@ mkdir -p /home/odroid/.npm-global/
 mkdir -p /home/odroid/.django-monitor/
 mkdir -p /home/odroid/.pm2/
 npm config set prefix '/home/odroid/.npm-global'
-echo "export NODE_PATH=/home/odroid/.django-monitor/node_modules" >> /home/odroid/.profile
-echo "export PATH=/home/odroid/.npm-global/bin:\$PATH" >> /home/odroid/.profile
-source /home/odroid/.profile
+runuser -l odroid -c "echo "export NODE_PATH=/home/odroid/.django-monitor/node_modules" >> /home/odroid/.profile"
+runuser -l odroid -c "echo "export PATH=/home/odroid/.npm-global/bin:\$PATH" >> /home/odroid/.profile"
+runuser -l odroid -c "source /home/odroid/.profile"
 
 chown -R odroid:odroid /home/odroid/.pm2/
 chown -R odroid:odroid /home/odroid/.npm-global/
