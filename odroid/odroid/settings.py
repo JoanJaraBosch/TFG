7f9d63e4d084ad4b28a6c578b49fd1d164af4e74
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-10-23 01:18:46 +0200

    Update settings.py

--- odroid/odroid/settings.py
+++ odroid/odroid/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'pages',
     'teacher',
     'student',
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
