2b4d13138286ced4b661550b2e3a12701d20627b
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-10-28 13:14:06 +0100

    Eliminars arxius innecessaris

--- odroid/odroid/odroid/teachers/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:teacher_id>/', views.teacher, name="teacher")
-]
--- odroid/odroid/teacher/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Teacher
-# Register your models here.
-
-class TeacherAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
-
-admin.site.register(Teacher, TeacherAdmin)
-
