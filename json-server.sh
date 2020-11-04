c6ce27db4b17d612233002fbc7b31dcbae056cd5
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2020-11-04 16:53:30 +0100

    Update json-server.sh

--- json-server.sh
+++ json-server.sh
@@ -7,7 +7,7 @@
 cpu=$(ps -A -o pcpu | tail -n+2 | paste -sd+ | bc)
 mem=$(free -m | awk '{print $7}' | head -2 | tail -1)
 mem_avail=$(df -t ext4 --output=used,avail | head -2 | tail -1)
-temp=$(sensors | tail -2 | head -1 | cut -d " " -f 9,13)
+temp=$(sensors | tail -2 | head -1 | cut -d " " -f 9-)
 load=$(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
 cpunum=$(lscpu | head -3 | tail -1 | cut -d " " -f 15-)
 entropy=$(cat /proc/sys/kernel/random/entropy_avail)
@@ -18,7 +18,9 @@ writeread=$(iostat | tail -n +7 | awk '{r+=$5}{w+=$6}END{print w" "r}')
 
 for i in /sys/class/net/*; do RX=$(cat $i/statistics/tx_bytes); TX=$(cat $i/statistics/rx_bytes); network=$(echo "_"$(basename $i)": "$RX" "$TX" "$network) ; done
 
-temp=$(echo "${temp//[+,),C,°]/}")
+temp=$(echo "${temp//[+,),(,=,C,a-z,°]/}")
+temp=$(echo $temp | tr -s " ")
+
 load=$(echo $load" "$cpunum)
 up=$(echo $days"-"$min"-"$sec)
 
